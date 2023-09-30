713516bf8d3d6b84ae48b459855f25d31d3425a3
Nicollas Lopes <collascollas29@gmail.com>
2023-09-29 23:30:45 -0300

    Continuando tela de login

--- LojinhaAngular/src/app/components/home/home.component.ts
+++ LojinhaAngular/src/app/components/home/home.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon'
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  private formBuilder: FormBuilder = new FormBuilder();
-  
+
+export class HomeComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    senha: new FormControl('', Validators.required),
+  });
+
+  get usuario() {
+    return this.form.controls['usuario'].value;
+  }
+  get senha() {
+    return this.form.controls['senha'].value;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public login() {
+    if(this.validateForm()){
+      console.log(this.usuario);
+    }
+    else{
+      alert('Preencha todos os campos corretamente');
+    }
+    this.clearForm();
+  }
+
+  private validateForm(){
+    return this.form.valid;
+  }
+
+  private clearForm(){
+    this.form.reset();
+  }
 }
